5807577c77a9f6a11a55a6b4892e435f5577d542
kermage <genealyson.torcende@gmail.com>
2021-02-21 00:58:40 +0800

    prepare the items; multi-dimensional array

--- Menu.php
+++ Menu.php
@@ -21,7 +21,38 @@ class Menu {
 
 		$this->object = wp_get_nav_menu_object( $locations[ $location ] ?: $location );
 
-		$this->items = wp_get_nav_menu_items( $this->object );
+		$items = wp_get_nav_menu_items( $this->object );
+
+		$this->items = $this->prepare( $items );
+
+	}
+
+
+	private function prepare( $items, $parent = 0 ) {
+
+		$prepared = [];
+
+		foreach ( $items as $item ) {
+			if ( (int) $item->menu_item_parent === $parent ) {
+				$children = $this->prepare( $items, (int) $item->ID );
+
+				$prepared[] = [
+					'id'       => $item->ID,
+					'slug'     => $item->post_name,
+					'parent'   => $item->menu_item_parent,
+					'label'    => $item->title,
+					'url'      => $item->url,
+					'target'   => $item->target,
+					'title'    => $item->attr_title,
+					'info'     => $item->description,
+					'classes'  => $item->classes,
+					'xfn'      => $item->xfn,
+					'children' => $children,
+				];
+			}
+		}
+
+		return $prepared;
 
 	}
 
