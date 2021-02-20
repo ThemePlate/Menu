39ea729b39d024a3cef9872b1173122062e18413
kermage <genealyson.torcende@gmail.com>
2021-02-21 00:27:28 +0800

    the base class actions

--- Menu.php
+++ Menu.php
@@ -10,4 +10,33 @@
 namespace ThemePlate;
 
 class Menu {
+
+	private $object;
+	private $items = [];
+
+
+	public function __construct( $location ) {
+
+		$locations = get_nav_menu_locations();
+
+		$this->object = wp_get_nav_menu_object( $locations[ $location ] ?: $location );
+
+		$this->items = wp_get_nav_menu_items( $this->object );
+
+	}
+
+
+	public function get() {
+
+		return $this->object;
+
+	}
+
+
+	public function get_items() {
+
+		return $this->items;
+
+	}
+
 }
